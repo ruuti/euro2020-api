8fc13531b2d6503827c596d0ae91a1293146bc2c
ruuti <17232523+ruuti@users.noreply.github.com>
2021-05-15 22:26:02 +0200

    Deploy workflow (#5)

---
+++ pm2.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "apps": [ {
+    "name": "api",
+    "script": "dist/index.js",
+    "node_args": "-r dotenv/config"
+  } ]
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import "dotenv/config";
 import server from "server";
 
