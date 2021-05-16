2ff0b5c3e850e4df39c43aca57d25566a10fb588
ruuti <17232523+ruuti@users.noreply.github.com>
2021-05-16 15:59:25 +0200

    Treat trailing slash uri resources differently than without trailing slash (#7)

--- src/routes/groups.js
+++ src/routes/groups.js
@@ -3,7 +3,7 @@ import apicache from "apicache";
 import { fbd } from "services";
 import Standings from "models/Standings";
 
-const router = new Router();
+const router = new Router({ "strict": true });
 const cache = apicache.middleware;
 
 /**
@@ -11,7 +11,7 @@ const cache = apicache.middleware;
  * @param {object} req HTTP request argument
  * @param {object} res HTTP response argument
  */
-router.get("/", cache("5 minutes"), async (req, res) => {
+router.get("/groups", cache("5 minutes"), async (req, res) => {
   try {
     const standingsResponse = await fbd.getStandings();
     const responseData = new Standings(standingsResponse.data);
