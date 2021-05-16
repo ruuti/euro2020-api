0aca7ed6c176f0a6b4c45fb1219c522dfeceacce
ruuti <17232523+ruuti@users.noreply.github.com>
2021-05-16 16:07:02 +0200

    Ignore query params when caching resources (#10)

--- src/routes/groups.js
+++ src/routes/groups.js
@@ -1,23 +1,30 @@
 import { Router } from "express";
-import apicache from "apicache";
 import { fbd } from "services";
 import { Standings } from "models";
 import logger from "logger";
+import { getFromCache, saveToCache } from "cache";
 
 const router = new Router({ "strict": true });
-const cache = apicache.middleware;
 
 /**
  * GET / route that responds with group standings data
  * @param {object} req HTTP request argument
  * @param {object} res HTTP response argument
  */
-router.get("/groups", cache("5 minutes"), async (req, res) => {
+router.get("/groups", async (req, res) => {
+  const cacheKey = "groupsList";
+  const cachedResponse = await getFromCache(cacheKey);
+
+  if (cachedResponse) {
+    return res.send(cachedResponse);
+  }
+
   try {
     const standingsResponse = await fbd.getStandings();
-    const responseData = new Standings(standingsResponse.data);
+    const response = new Standings(standingsResponse.data);
 
-    return res.send(responseData);
+    await saveToCache(cacheKey, response, "5 minutes");
+    return res.send(response);
   } catch (error) {
     logger.error(error.message);
     return res.status(500).send({ "error": "Unknown error" });
--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,22 +1,29 @@
 import { Router } from "express";
-import apicache from "apicache";
 import { fbd } from "services";
 import { Matches } from "models";
 import logger from "logger";
+import { getFromCache, saveToCache } from "cache";
 
 const router = new Router({ "strict": true });
-const cache = apicache.middleware;
 
 /**
  * GET / route that responds with matches array
  * @param {object} req HTTP request argument
  * @param {object} res HTTP response argument
  */
-router.get("/matches", cache("5 minutes"), async (req, res) => {
+router.get("/matches", async (req, res) => {
+  const cacheKey = "matchesList";
+  const cachedResponse = await getFromCache(cacheKey);
+
+  if (cachedResponse) {
+    return res.send(cachedResponse);
+  }
+
   try {
     const matchesResponse = await fbd.getMatches();
     const response = new Matches(matchesResponse.data);
 
+    await saveToCache(cacheKey, response, "5 minutes");
     return res.send(response);
   } catch (error) {
     logger.error(error.message);
