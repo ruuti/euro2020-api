10606081532ba713767e493ab033fdce2e890d37
ruuti <17232523+ruuti@users.noreply.github.com>
2021-05-16 20:40:34 +0200

    Cache middleware for groups and matches endpoints (#11)

--- src/routes/groups.js
+++ src/routes/groups.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { fbd } from "services";
 import { Standings } from "models";
 import logger from "logger";
-import { getFromCache, saveToCache } from "cache";
+import cacheMiddleware from "middlewares/cacheMiddleware";
 
 const router = new Router({ "strict": true });
 
@@ -11,23 +11,15 @@ const router = new Router({ "strict": true });
  * @param {object} req HTTP request argument
  * @param {object} res HTTP response argument
  */
-router.get("/groups", async (req, res) => {
-  const cacheKey = "groupsList";
-  const cachedResponse = await getFromCache(cacheKey);
-
-  if (cachedResponse) {
-    return res.send(cachedResponse);
-  }
-
+router.get("/groups", cacheMiddleware("5 minutes"), async (req, res) => {
   try {
     const standingsResponse = await fbd.getStandings();
     const response = new Standings(standingsResponse.data);
 
-    await saveToCache(cacheKey, response, "5 minutes");
-    return res.send(response);
+    return res.json(response);
   } catch (error) {
     logger.error(error.message);
-    return res.status(500).send({ "error": "Unknown error" });
+    return res.status(500).json({ "error": "Unknown error" });
   }
 });
 
